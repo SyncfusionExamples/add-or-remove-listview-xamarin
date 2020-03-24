9461fd209a55ffaa190bf76e31468937aeae10ee
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-24 18:02:34 +0530

    XAMARIN-21746 - Sample committed.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ContactsViewModel.cs
@@ -0,0 +1,217 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class ContactsViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        public ObservableCollection<Contacts> contactsinfo { get; set; }
+        #endregion
+
+        #region Commands
+
+        public Command AddCommand { get; set; }
+        public Command DeleteCommand { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public ContactsViewModel()
+        {
+            contactsinfo = new ObservableCollection<Contacts>();
+            AddCommand = new Command(OnAddTapped);
+            DeleteCommand = new Command(OnDeleteTapped);
+            GenerateInfo();
+        }
+
+        public void GenerateInfo()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                var contact = new Contacts(CustomerNames[i], r.Next(720, 799).ToString() + " - " + r.Next(3010, 3999).ToString());
+                contact.ContactImage = ImageSource.FromResource("ListViewXamarin.Images.Image" + r.Next(0, 28) + ".png");
+                contactsinfo.Add(contact);
+            }
+        }
+
+        #endregion
+
+        #region Command methods
+
+        private void OnDeleteTapped(object obj)
+        {
+            if (contactsinfo.Count > 0)
+                contactsinfo.Remove(contactsinfo[contactsinfo.Count - 1]);
+            else
+                App.Current.MainPage.DisplayAlert("Alert", "There is no item in the list", "OK");
+        }
+
+        private void OnAddTapped(object obj)
+        {
+            var details = new Contacts()
+            {
+                ContactNumber = random.Next(100, 400).ToString() + "-" + random.Next(500, 800).ToString() + "-" + random.Next(1000, 2000).ToString(),
+                ContactName = CustomerNames[random.Next(1, 50)],
+                ContactImage = ImageSource.FromResource("ListViewXamarin.Images.Image" + random.Next(0, 28) + ".png", typeof(MainPage)),
+            };
+            contactsinfo.Add(details);
+        }
+        #endregion
+
+        #region Fields
+
+        Random random = new Random();
+
+        string[] CustomerNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Michael",
+            "Oscar",
+            "Ralph",
+            "Torrey",
+            "William",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson"    ,
+            "Mason  "    ,
+            "Liam   "    ,
+            "Jacob  "    ,
+            "Jayden "    ,
+            "Ethan  "    ,
+            "Noah   "    ,
+            "Lucas  "    ,
+            "Logan  "    ,
+            "Caleb  "    ,
+            "Caden  "    ,
+            "Jack   "    ,
+            "Ryan   "    ,
+            "Connor "    ,
+            "Michael"    ,
+            "Elijah "    ,
+            "Brayden"    ,
+            "Benjamin"   ,
+            "Nicholas"   ,
+            "Alexander"  ,
+            "William"    ,
+            "Matthew"    ,
+            "James  "    ,
+            "Landon "    ,
+            "Nathan "    ,
+            "Dylan  "    ,
+            "Evan   "    ,
+            "Luke   "    ,
+            "Andrew "    ,
+            "Gabriel"    ,
+            "Gavin  "    ,
+            "Joshua "    ,
+            "Owen   "    ,
+            "Daniel "    ,
+            "Carter "    ,
+            "Tyler  "    ,
+            "Cameron"    ,
+            "Christian"  ,
+            "Wyatt  "    ,
+            "Henry  "    ,
+            "Eli    "    ,
+            "Joseph "    ,
+            "Max    "    ,
+            "Isaac  "    ,
+            "Samuel "    ,
+            "Anthony"    ,
+            "Grayson"    ,
+            "Zachary"    ,
+            "David  "    ,
+            "Christopher",
+            "John   "    ,
+            "Isaiah "    ,
+            "Levi   "    ,
+            "Jonathan"   ,
+            "Oliver "    ,
+            "Chase  "    ,
+            "Cooper "    ,
+            "Tristan"    ,
+            "Colton "    ,
+            "Austin "    ,
+            "Colin  "    ,
+            "Charlie"    ,
+            "Dominic"    ,
+            "Parker "    ,
+            "Hunter "    ,
+            "Thomas "    ,
+            "Alex   "    ,
+            "Ian    "    ,
+            "Jordan "    ,
+            "Cole   "    ,
+            "Julian "    ,
+            "Aaron  "    ,
+            "Carson "    ,
+            "Miles  "    ,
+            "Blake  "    ,
+            "Brody  "    ,
+            "Adam   "    ,
+            "Sebastian"  ,
+            "Adrian "    ,
+            "Nolan  "    ,
+            "Sean   "    ,
+            "Riley  "    ,
+            "Bentley"    ,
+            "Xavier "    ,
+            "Hayden "    ,
+            "Jeremiah"   ,
+            "Jason  "    ,
+            "Jake   "    ,
+            "Asher  "    ,
+            "Micah  "    ,
+            "Jace   "    ,
+            "Brandon"    ,
+            "Josiah "    ,
+            "Hudson "    ,
+            "Nathaniel"  ,
+            "Bryson "    ,
+            "Ryder  "    ,
+            "Justin "    ,
+            "Bryce  "    ,
+        };
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
